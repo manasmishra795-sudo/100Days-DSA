a273f48e88655f36ff7c52cb069d58913b4d1202
Manas Mishra <manasmishra795@gmail.com>
2026-02-26 22:39:47 +0530

    Add files via upload

---
+++ Q50.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct ListNode {
+    int val;
+    struct ListNode *next;
+
+};
+struct ListNode *detectCycle(struct ListNode *head) 
+{
+    struct ListNode *fast=head;
+    struct ListNode *slow=head;
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        fast=fast->next->next;
+        slow=slow->next;
+        if(fast==slow)
+        {
+            slow=head;
+            while(slow!=fast)
+            {
+                slow=slow->next;
+                fast=fast->next;
+            }
+            return slow;
+        }
+
+    }
+    return NULL;
+    
+}
