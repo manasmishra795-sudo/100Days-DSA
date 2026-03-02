007e6bd5732a4a7898fb2f24766609f245f5293f
Manas Mishra <manasmishra795@gmail.com>
2026-03-02 22:19:38 +0530

    Add files via upload

---
+++ Q46.c
@@ -0,0 +1,26 @@
+// list cycle detection
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+struct ListNode {
+    int val;
+    struct ListNode *next;
+
+};
+bool hasCycle(struct ListNode *head) {
+
+    struct ListNode *fast=head;
+    struct ListNode *slow=head;
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        fast=fast->next->next;
+        slow=slow->next;
+        if(fast==slow)
+        {
+            return true;
+        }
+
+    }
+    return false;
+    
+}
